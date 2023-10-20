a1e80b415ac19db7cbbd189b1257c783203344a3
JhoanLuna <johanluna20008@outlook.com>
2023-10-19 20:00:28 -0500

    configuracion vistas y middlewares
    se instala y configura nodemon, se configura las rutas hacia la carpeta views.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import express from 'express';
-
+import exphbs from 'express-handlebars';
+import path from 'path';
 //Initializations
 const app = express();
 
 //Settings
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'view'));
+app.engine('.hbs', exphbs({
+    extname: '.hbs',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    helpers: require('./lib/helpers')
+}));
+
 //Middlewares
+app.use(express.json()); //entender JSON que lleguen al servidor
+app.use(express.urlencoded({extended: false})); //cuando un formularo HTTP envia un dato, se puede leer el dato
 
 //Routes
 
